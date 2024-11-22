aeb7b83423637b172b7ba76756f2fd1e55c5fd8c
Billy <83192688+BDsProjects@users.noreply.github.com>
2024-11-21 23:17:07 -0500

    Added the tachometer gauge cpp file

---
+++ App_Design/CircularGauge.cpp
@@ -0,0 +1,7 @@
+// CircularGauge.cpp
+#include "CircularGauge.h"
+
+void DrawCircularGauge(const char* label, float value, float minValue, float maxValue,
+    float radius, ImU32 color, const char* format, float angleMin, float angleMax) {
+    // Function implementation (as provided)
+}
